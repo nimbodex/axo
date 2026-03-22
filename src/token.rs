e2affa7eb79ce3cf75a0591e62cd65742972eb89
Maksim Shirokov <nimbodex@yandex.ru>
2026-03-22 20:49:10 +0500

    Add base set of tokens

---
+++ src/token.rs
@@ -0,0 +1,37 @@
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum TokenType {
+    Illegal,
+    Eof,
+    Ident,
+    Int,
+
+    Assign,
+    Plus,
+
+    Comma,
+    Semicolon,
+
+    Lparen,
+    Rparen,
+    Lbrace,
+    Rbrace,
+
+    Function,
+    Let,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Token {
+    pub kind: TokenType,
+    pub literal: String,
+}
+
+impl Token {
+    pub fn new(kind: TokenType, literal: impl Into<String>) -> Self {
+        Self {
+            kind,
+            literal: literal.into(),
+        }
+    }
+}
+
